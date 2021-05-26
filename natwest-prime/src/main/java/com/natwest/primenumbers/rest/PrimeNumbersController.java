76ef84123dc2383c3e42b282bf06d6647b8fd326
Dev <Dev@192.168.1.101>
2021-05-26 22:31:56 +0100

    test

--- natwest-prime/src/main/java/com/natwest/primenumbers/rest/PrimeNumbersController.java
+++ natwest-prime/src/main/java/com/natwest/primenumbers/rest/PrimeNumbersController.java
@@ -2,11 +2,13 @@ package com.natwest.primenumbers.rest;
 
 import java.util.List;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +26,8 @@ public class PrimeNumbersController {
 	@Autowired
 	private PrimeNumberService primeNumberService;
 
+	private static final String VALIDATION_ERROR_MSG = "Initial Must be Greater Than One";
+
 	/**
 	 * Fetch all prime Numbers with the initial.
 	 * <code>http://.../v1/api/primes/{initial}</code> will return all prime
@@ -32,21 +36,28 @@ public class PrimeNumbersController {
 	 * @param initial
 	 * @return json of initial and prime numbers
 	 */
-	@RequestMapping(value = "/{initial}", method = RequestMethod.GET  ,produces = { "application/json", "application/xml" })
+	@RequestMapping(value = "/{initial}", method = RequestMethod.GET, produces = { "application/json",
+			"application/xml" })
 	public ResponseEntity<PrimeNumbersResponse> getAllPrimeNumbers(@PathVariable("initial") int initial) {
+		StopWatch watch = new StopWatch();
+		watch.start();
 		List<Integer> primeNumberList;
 		if (initial <= 1) {
-			return new ResponseEntity("Initial Must be Greater Than One", HttpStatus.OK);
+			LOGGER.warn("getAllPrimeNumbers REST Call {0} , {1}", VALIDATION_ERROR_MSG, initial);
+			return new ResponseEntity(VALIDATION_ERROR_MSG, HttpStatus.OK);
 		}
 
 		try {
 			primeNumberList = primeNumberService.getAllPrimeNumbers(initial);
 		} catch (Exception ex) {
-			LOGGER.warn("Exception raised getAllPrimeNumbers REST Call {0}", ex);
+			LOGGER.error("Exception raised getAllPrimeNumbers REST Call {0}", ex);
 			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 		final PrimeNumbersResponse primeNumbersResponse = new PrimeNumbersResponse(initial, primeNumberList);
+
+		watch.stop();
+		LOGGER.info("getAllPrimeNumbers Request Completed Successfully total Time Taken " + watch.getTime()+ " Millisecond");
 		return new ResponseEntity(primeNumbersResponse, HttpStatus.OK);
 
 	}
